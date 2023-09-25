a4caa9a2e4c10c6ec00211133b6b46bf48112774
Dima Lavrisiuk <dmitriyemigrant@gmail.com>
2023-09-25 18:06:21 +0300

    3-4: Using Array.from

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -3,12 +3,6 @@
  * @param {*[]} arr - the array of primitive values
  * @returns {*[]} - the new array with uniq values
  */
-export function uniq(arr) {
-  const arrayOfUniqueValues = [];
-
-  for (const [key] of new Set(arr).entries()) {
-    arrayOfUniqueValues.push(key)
-  }
-
-  return arrayOfUniqueValues;
+export function uniq(arr = []) {
+  return arr.length ? Array.from(new Set(arr).entries(), ([key]) => key) : [];
 }
