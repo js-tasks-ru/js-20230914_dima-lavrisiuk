fff5b61465131c1842eff5ff497b594f430081c4
Dima Lavrisiuk <dmitriyemigrant@gmail.com>
2023-10-02 16:01:25 +0300

    feat(5-1): Creating a NotificationMessage element

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,70 @@
-export default class NotificationMessage {
+import createElement from "../../assets/lib/create-element.js";
 
+export default class NotificationMessage {
+  static #current = null;
+  #element        = null;
+  #content        = null;
+  #type           = null;
+  #duration       = null;
+  #timeoutID      = null;
+
+  constructor(content = "", options = {}) {
+    this.content  = content;
+    this.type     = options?.type;
+    this.duration = options?.duration;
+    this.render();
+  }
+
+  static get current() { return NotificationMessage.#current; }
+  static set current(self) { NotificationMessage.#current = self; }
+
+  get element() { return this.#element; }
+
+  get content() { return this.#content; }
+  set content(content) { this.#content = (typeof content === "string") ? content : "Notification message!"; }
+
+  get type() { return this.#type; }
+  set type(string) { this.#type = (typeof string === "string") ? string : "error"; }
+
+  get duration() { return this.#duration; }
+  set duration(number) { this.#duration = Number.isInteger(number) ? number : 20; }
+
+  get timeoutID() { return this.#timeoutID; }
+  set timeoutID(fn) { this.#timeoutID = (typeof fn === "function") ? fn : (s) => s; }
+
+  show(node = null) {
+    if( NotificationMessage.current ) {
+      this.destroy.call(NotificationMessage.current);
+    }
+
+    NotificationMessage.current = this;
+    this.timeoutID = setTimeout(() => this.destroy(), this.duration);
+    (node || document.body).insertAdjacentElement('beforeend', this.element);
+  }
+
+  render() {
+    this.#element = createElement(this.template());
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+    clearTimeout(this.timeoutID);
+    NotificationMessage.current = null;
+  }
+
+  template() {
+    return `
+      <div class="notification ${ this.type }" style="--value:${ this.duration / 1000 }s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${ this.type }</div>
+          <div class="notification-body">${ this.content }</div>
+        </div>
+      </div>
+    `;
+  }
 }
